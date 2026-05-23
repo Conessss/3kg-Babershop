84ea9cd8e7bac243a5cfbe817a779e442078d54e
Mark Lawrence Tatel <marklawrencetatel2005@gmail.com>
2026-05-24 01:28:31 +0800

    add cors support

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// ADD CORS HERE - before express.json()
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.static('public'));
