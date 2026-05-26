d9e9ad5ac0ab80006b501a51abfb6c745af38b84
Mark Lawrence Tatel <marklawrencetatel2005@gmail.com>
2026-05-26 09:03:15 +0800

    changed the contact header, added admin

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const bookingSchema = new mongoose.Schema({
     time: String,
     service: String,
     branch: String,
-    address: String,        // ← ADDED: for Home Visit bookings
+    address: String,        
     visitType: String,
     details: String,
     createdAt: { type: Date, default: Date.now }
